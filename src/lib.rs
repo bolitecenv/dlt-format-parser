c9fa0501dd87f3d6aec8983f668b2769d35bdbe3
Toshinori Tamashiro <boliteconline@gmail.com>
2025-07-03 03:06:13 +0900

    fix build error

--- src/lib.rs
+++ src/lib.rs
@@ -169,7 +169,7 @@ fn dlt_service_parser(cursor: &mut Cursor<Vec<u8>>, len: usize) -> MessageList {
     let mut payload = vec![0u8; len];
 
     match cursor.read_exact(&mut payload_header) {
-        Ok(_) = > {
+        Ok(_) => {
             println!("Sucess");
         }
         Err(e) => {
@@ -177,7 +177,7 @@ fn dlt_service_parser(cursor: &mut Cursor<Vec<u8>>, len: usize) -> MessageList {
         }
     }
     match cursor.read_exact(&mut payload) {
-        Ok(_) = > {
+        Ok(_) => {
             println!("Sucess");
         }
         Err(e) => {
@@ -302,18 +302,18 @@ pub struct MessageList {
 
 impl MessageList {
     fn parse_type(type_byte: u32) -> Option<MessageType> {
-        if type_byte & 0x10 != 0 {return Some(Message::Bool); }
-        if type_byte & 0x20 != 0 {return Some(Message::Signed); }
-        if type_byte & 0x40 != 0 {return Some(Message::Unsigned); }
-        if type_byte & 0x80 != 0 {return Some(Message::Float); }
-        if type_byte & 0x100 != 0 {return Some(Message::Array); }
-        if type_byte & 0x200 != 0 {return Some(Message::String); }
-        if type_byte & 0x400 != 0 {return Some(Message::Raw); }
-        if type_byte & 0x800 != 0 {return Some(Message::VariableInfo); }
-        if type_byte & 0x1000 != 0 {return Some(Message::FixedPoint); }
-        if type_byte & 0x2000 != 0 {return Some(Message::TraceInfo); }
-        if type_byte & 0x4000 != 0 {return Some(Message::Struct); }
-        if type_byte & 0x8000 != 0 {return Some(Message::StringCoding); }
+        if type_byte & 0x10 != 0 {return Some(MessageType::Bool); }
+        if type_byte & 0x20 != 0 {return Some(MessageType::Signed); }
+        if type_byte & 0x40 != 0 {return Some(MessageType::Unsigned); }
+        if type_byte & 0x80 != 0 {return Some(MessageType::Float); }
+        if type_byte & 0x100 != 0 {return Some(MessageType::Array); }
+        if type_byte & 0x200 != 0 {return Some(MessageType::String); }
+        if type_byte & 0x400 != 0 {return Some(MessageType::Raw); }
+        if type_byte & 0x800 != 0 {return Some(MessageType::VariableInfo); }
+        if type_byte & 0x1000 != 0 {return Some(MessageType::FixedPoint); }
+        if type_byte & 0x2000 != 0 {return Some(MessageType::TraceInfo); }
+        if type_byte & 0x4000 != 0 {return Some(MessageType::Struct); }
+        if type_byte & 0x8000 != 0 {return Some(MessageType::StringCoding); }
         None
     }
 
@@ -367,7 +367,7 @@ impl MessageList {
                     println!("Invalid message type: {}", type_byte);
                     continue;
                 }
-            }
+            };
 
             match message_type {
                 MessageType::String => {
